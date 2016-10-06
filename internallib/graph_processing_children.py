5f36a8483eeb2b1819a981756d185911cc352384
Alisson Oldoni <aoldoni@gmail.com>
2016-10-06 20:30:27 +1100

    Ading nofollow appos

--- internallib/graph_processing_children.py
+++ internallib/graph_processing_children.py
@@ -9,7 +9,7 @@ from functools import wraps
 class Obj(RuleApplier):
     def __init__(self):
         RuleApplier.__init__(self)
-        self.tags_to_be_removed = set(['punct', 'det', ' ', ''])
+        self.tags_to_be_removed = set(['punct', 'appos', 'det', ' ', ''])
         return
 
     @RuleApplier.register_function
@@ -19,6 +19,13 @@ class Obj(RuleApplier):
 
             The dependency parser puts not the action the improves something as a parent of the word "improves" in the in the tree, and adds to it the relcl relation.
             This method adjusts the tree, bringing the node above under "improves".
+
+            2) Consider the following sentence:
+            "Evaluation on the ACE 2003 corpus shows that, compared with a baseline coreference resolution system of no explicit anaphoricity determination, their method improves the performance by 2.8, 2.2 and 4.5 to 54.5, 64.0 and 60.8 in F1-measure (due to the gain in precision) on the NWIRE, NPAPER and BNEWS domains, respectively, via careful determination of an anaphoricity threshold with proper constraint-based representation and global optimization."
+            "Adding latent states to the smoothing model further improves the POS tagging accuracy (Huang and Yates, 2012)."
+            
+            The "appos" relation prints further information on the noun that is part of the "obj" node. http://universaldependencies.org/u/dep/appos.html
+            One can remove it as in all observed cases the extra information wasn't really relevant (wither it were citations, or long subsequent clauses)
         """
 
         node_set = set(node_set) - self.tags_to_be_removed
@@ -65,22 +72,10 @@ class Obj(RuleApplier):
 
     @RuleApplier.register_function
     def break_into_multiple_relations(self, root, node_set, spacy_tree):
-        """1) TODO: Consider the following sentence:
+        """1) Consider the following sentence:
             "The spirit of this work more closely resembles that of Finkel and Manning (2009) , which improves both parsing and named entity recognition by combining the two tasks."
 
             It should yield 2 relations as it improves "parsing" AND "named entity recognition"
-        """
-
-        return root, node_set, spacy_tree
-
-    @RuleApplier.register_function
-    def no_follow_appos(self, root, node_set, spacy_tree):
-        """1) TODO: Consider the following sentence:
-            "Evaluation on the ACE 2003 corpus shows that, compared with a baseline coreference resolution system of no explicit anaphoricity determination, their method improves the performance by 2.8, 2.2 and 4.5 to 54.5, 64.0 and 60.8 in F1-measure (due to the gain in precision) on the NWIRE, NPAPER and BNEWS domains, respectively, via careful determination of an anaphoricity threshold with proper constraint-based representation and global optimization."
-            "Adding latent states to the smoothing model further improves the POS tagging accuracy (Huang and Yates, 2012)."
-            
-            The "appos" relation prints further information on the noun that is part of the "obj" node. http://universaldependencies.org/u/dep/appos.html
-            One can remove it as in all observed cases the extra information wasn't really relevant (wither it were citations, or long subsequent clauses)
         """
 
         return root, node_set, spacy_tree
