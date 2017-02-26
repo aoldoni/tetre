f8d6be0f2ac894512540c39f65fe7835acc9db6f
Alisson Oldoni <aoldoni@gmail.com>
2017-02-26 23:50:12 +1100

    Refactoring brat_to_stanford extraction process

--- lib/brat_to_stanford/extract.py
+++ lib/brat_to_stanford/extract.py
@@ -1,76 +1,88 @@
 import os
 
-from lib.directories import *
-from lib.openie import openie_to_pretty
+from directories import dirs
+from openie import openie_to_pretty
 
 
-def argparser():
-    import argparse
-    ap = argparse.ArgumentParser(description='Find relations in a file in a directory',
-                                 usage='%(prog)s [OPTIONS] DIRECTORY_PLEASE_NAME_FULL_INPUT_PATH')
-    ap.add_argument('directory')
-    ap.add_argument('-u', '--use_model', action='store_true',
-                    help='uses Relation Extractor with the trained model')
-    ap.add_argument('-b', '--bulk_processing', action='store_true',
-                    help='tries Bulk processing')
+def run_relations_separate_output(argv):
 
-    return ap
-
-
-def run_relations_separate_output(args):
-
-    for fn in os.listdir(args.directory+raw_input):
-        if (fn == ".DS_Store"):
+    for fn in os.listdir(dirs['raw_input']['path']):
+        if fn == ".DS_Store":
             continue
 
-        name = args.directory + raw_input + fn
+        name = dirs['raw_input']['path'] + fn
 
-        if (args.use_model == False):
-            print "Running Stanford's OpenIE (1 per file mode)..."
+        if not argv.brat_to_stanford_use_model:
+            print("Running Stanford's OpenIE (1 per file mode)...")
             
-            output_name = args.directory+output_openie + fn + '.tsv.original'
-            output_processed = args.directory+output_openie + fn + '.tsv.easy'
+            output_name = dirs['output_openie']['path'] + fn + '.tsv.original'
+            output_processed = dirs['output_openie']['path'] + fn + '.tsv.easy'
 
-            print "Will write to:"
-            print output_name
-            print output_processed
+            print("Will write to files:")
+            print(output_name)
+            print(output_processed)
 
-            os.system('java -cp stanford/stanford-corenlp-full-2015-12-09/*:stanford/stanford-ner-2015-12-09/lib/* -Xmx8g edu.stanford.nlp.naturalli.OpenIE -props '+config+'dblp-pipeline-openie.properties ' + name + ' 1>' + output_name)
+            os.system(''.join(['java -cp ',
+                               dirs['stanford_corenlp_path']['path'] + '*:',
+                               dirs['stanford_ner_path']['path'] + 'lib/*',
+                               '-Xmx8g ',
+                               'edu.stanford.nlp.naturalli.OpenIE ',
+                               '-props ' + dirs['config']['path'] + 'pipeline-openie.properties ',
+                               name,
+                               ' 1>' + output_name]))
 
-            with open(output_name, 'r') as input, \
-                open(output_processed, 'w') as output:
-                for line in input.readlines():
+            with open(output_name, 'r') as input_file, open(output_processed, 'w') as output_file:
+                for line in input_file.readlines():
                     output_line_split = line.split('\t')
                     output_line = openie_to_pretty(output_line_split)
-                    output.write(output_line + "\n")
+                    output_file.write(output_line + "\n")
 
-        elif (args.use_model == True):
-            print "Running Stanford's Relation Extractor (1 per file mode)..."
-            os.system('java -cp stanford/stanford-corenlp-full-2015-12-09/*:stanford/stanford-ner-2015-12-09/lib/* -Xmx8g edu.stanford.nlp.pipeline.StanfordCoreNLP -props '+config+'dblp-pipeline.properties -file ' + name)
+        else:
+            print("Running Stanford's Relation Extractor (1 per file mode)...")
+            os.system(''.join(['java -cp ',
+                               dirs['stanford_corenlp_path']['path'] + '*:',
+                               dirs['stanford_ner_path']['path'] + 'lib/* ',
+                               '-Xmx8g ',
+                               'edu.stanford.nlp.pipeline.StanfordCoreNLP ',
+                               '-props ' + dirs['config']['path'] + 'pipeline.properties ',
+                               '-file ', name]))
 
     return
 
 
-def run_relations(args):
+def run_relations(argv):
     names = []
     filename = 'filelist.txt'
 
-    for fn in os.listdir(args.directory+raw_input):
-        if (fn == ".DS_Store"):
+    for fn in os.listdir(argv.directory + dirs['raw_input']['path']):
+        if fn == ".DS_Store":
             continue
-        names.append(args.directory +raw_input+ fn)
+
+        names.append(dirs['raw_input']['path'] + fn)
 
     with open(filename, 'w') as output:
         data = "\n".join(names)
         output.write(data)
 
-    if (args.use_model == False):
-        print "Running Stanford's OpenIE... (Bulk mode)"
-        os.system('java -cp stanford/stanford-corenlp-full-2015-12-09/*:stanford/stanford-ner-2015-12-09/lib/* -Xmx8g edu.stanford.nlp.naturalli.OpenIE -props '+config+'dblp-pipeline-openie.properties -filelist ' + filename + ' 1>'+args.directory+output_openie+'/output.tsv')
+    if not argv.brat_to_stanford_use_model:
+        print("Running Stanford's OpenIE... (Bulk mode)")
+        os.system(''.join(['java -cp ',
+                           dirs['stanford_corenlp_path']['path'] + '*:',
+                           dirs['stanford_ner_path']['path'] + 'lib/* ',
+                           '-Xmx8g ',
+                           'edu.stanford.nlp.naturalli.OpenIE ',
+                           '-props ' + dirs['config']['path'] + 'pipeline-openie.properties ',
+                           '-filelist ' + filename + ' 1>' + dirs['output_openie']['path'] + '/output.tsv']))
 
-    elif (args.use_model == True):
-        print "Running Stanford's Relation Extractor... (Bulk mode)"
-        os.system('java -cp stanford/stanford-corenlp-full-2015-12-09/*:stanford/stanford-ner-2015-12-09/lib/* -Xmx8g edu.stanford.nlp.pipeline.StanfordCoreNLP -props '+config+'dblp-pipeline.properties -filelist ' + filename)
+    else:
+        print("Running Stanford's Relation Extractor... (Bulk mode)")
+        os.system(''.join(['java -cp ',
+                           dirs['stanford_corenlp_path']['path'] + '*:',
+                           dirs['stanford_ner_path']['path'] + 'lib/* ',
+                           '-Xmx8g ',
+                           'edu.stanford.nlp.pipeline.StanfordCoreNLP ',
+                           '-props ' + dirs['config']['path'] + 'pipeline.properties ',
+                           '-filelist ' + filename]))
 
     os.remove(filename)
 
@@ -78,12 +90,8 @@ def run_relations(args):
 
 
 def run(argv):
-    args = argparser().parse_args(argv[1:])
 
-    if (not args.directory.endswith("/")):
-        args.directory = args.directory + "/"
-
-    if (args.bulk_processing):
-        run_relations(args)
+    if argv.brat_to_stanford_bulk_processing:
+        run_relations(argv)
     else:
-        run_relations_separate_output(args)
+        run_relations_separate_output(argv)
